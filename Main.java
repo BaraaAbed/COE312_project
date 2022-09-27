bf82070d540383e88cdfa55a224d5897cfae5f7e
ahmad-sm02 <66691193+ahmad-sm02@users.noreply.github.com>
2022-09-27 21:07:40 +0400

    you are die

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("TESTING 123");
         System.out.println("haha i am here");
+        System.out.println("Why am i here");
     }
 }
