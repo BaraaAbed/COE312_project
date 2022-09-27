99c23e283fe8c3b73d3d4d5b5501241789735170
BaraaAbed <64694694+BaraaAbed@users.noreply.github.com>
2022-09-27 21:11:02 +0400

    NOICE

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("TESTING 123");
         System.out.println("haha i am here");
         System.out.println("Why am i here");
+        System.out.println("Just to suffer?");
     }
 }
