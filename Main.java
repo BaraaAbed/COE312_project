8a8d239dcd733088920c390a786f32fc94a48211
JawadZ02 <jawadzaabalawi@gmail.com>
2022-10-03 13:17:58 +0400

    Jawad testing

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("TESTING 123");
         System.out.println("haha i am here");
         System.out.println("testing lol");
+        int x=0;
+        try{
+            x = System.in.read();
+        }
+        catch(IOException e){
+
+        }
+        System.out.println("Your value is: "+(char)x);
     }
 }
