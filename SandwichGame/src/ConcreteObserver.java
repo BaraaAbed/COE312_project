6623a1dc316cb57b52f2e5ed0c28568c309b4a78
JawadZ02 <jawadzaabalawi@gmail.com>
2022-11-20 18:18:36 +0400

    modification to observer pattern

--- SandwichGame/src/ConcreteObserver.java
+++ SandwichGame/src/ConcreteObserver.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class ConcreteObserver implements Observer {
+    private ArrayList<ConcreteSubject> subjects;
+
+    public ConcreteObserver(ArrayList<ConcreteSubject> subjects) {
+        subjects = new ArrayList<ConcreteSubject>();
+
+        for (int i =0; i < subjects.size(); i++) {
+            subjects.get(i).registerObserver(this);
+        }
+    }
+    
     public void update() {
-        // implement in child
+        // implemented in child
     }
 }
